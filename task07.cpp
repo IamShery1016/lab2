e29de33aa0fa21d1b3656430b6fd89aeab8003e2
IamShery1016 <120797383+IamShery1016@users.noreply.github.com>
2022-12-20 04:38:07 -0800

    Add files via upload

---
+++ task07.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+main(){
+cout << "   -------------------------------- " <<endl;
+cout << "      O     ^__^            " <<endl;
+cout << "       O    (OO)| ______  /" <<endl;
+cout << "            (  )/       )_/ " <<endl;
+cout << "                ||____W| " <<endl;
+cout << "                ||    || " <<endl;
+}
+
